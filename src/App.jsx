42f55ea7fe69ef90be0bceaddf05003be4a5aafe
imtoufiq2 <124362024+imtoufiq2@users.noreply.github.com>
2024-08-20 17:33:09 +0530

    remove the private routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,77 +1,10 @@
-import {
-  createBrowserRouter,
-  Navigate,
-  RouterProvider,
-} from "react-router-dom";
-import PropTypes from "prop-types";
-
+import Routers from "./routers";
 import ThemeCustomization from "./themes";
-import SignIn from "./pages/auth/SignIn";
-import ResetPassword from "./pages/auth/reset_password";
-import Verify from "./pages/auth/Verify";
-import AssociatedCompanies from "./pages/AssociatedCompanies";
-import Department from "./pages/Department";
-import Reports from "./pages/Reports";
-import Meetings from "./pages/Meetings";
-
-function isUserAuthenticated() {
-  // return !!localStorage.getItem("authToken");
-  return true;
-}
-
-function PrivateRoute({ children }) {
-  const isUserAuthenticatedUser = isUserAuthenticated();
-  return isUserAuthenticatedUser ? children : <Navigate to="/sign-in" />;
-}
-// PropTypes validation for PrivateRoute
-PrivateRoute.propTypes = {
-  children: PropTypes.node.isRequired,
-};
-
-const appLayout = createBrowserRouter([
-  {
-    path: `/`,
-    children: [
-      {
-        path: "/",
-        element: <AssociatedCompanies />,
-      },
-      {
-        path: "sign-in",
-        element: <SignIn />,
-      },
-      {
-        path: "verify-otp",
-        element: <Verify />,
-      },
-      {
-        path: "forgot-password",
-        element: <ResetPassword />,
-      },
-      {
-        path: "department/:id",
-        element: <Department />,
-      },
-      {
-        path: "meetings/:id",
-        element: <Meetings />,
-      },
-      {
-        path: "reports/:id",
-        element: <Reports />,
-      },
-    ],
-  },
-  {
-    path: "*",
-    element: <>No page found</>,
-  },
-]);
 
 function App() {
   return (
     <ThemeCustomization>
-      <RouterProvider router={appLayout} />
+      <Routers />
     </ThemeCustomization>
   );
 }
---
+++ src/routers/index.jsx
@@ -0,0 +1,25 @@
+import { Routes, Route } from "react-router-dom";
+import AssociatedCompanies from "../pages/AssociatedCompanies";
+import SignIn from "../pages/auth/SignIn";
+import VerifyMobile from "../pages/auth/Verify";
+import ResetPassword from "../pages/auth/reset_password";
+import Department from "../pages/Department";
+import Meetings from "../pages/Meetings";
+import Reports from "../pages/Reports";
+
+const Routers = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<AssociatedCompanies />} />
+      <Route path="/sign-in" element={<SignIn />} />
+      <Route path="/verify-otp" element={<VerifyMobile />} />
+      <Route path="/forgot-password" element={<ResetPassword />} />
+      <Route path="/department/:id" element={<Department />} />
+      <Route path="/meetings/:id" element={<Meetings />} />
+      <Route path="/reports/:id" element={<Reports />} />
+      <Route path="/*" element={<>No page found</>} />
+    </Routes>
+  );
+};
+
+export default Routers;
