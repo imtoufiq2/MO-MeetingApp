94f96425a84c824156503f5fa4d0e0c448564811
imtoufiq2 <124362024+imtoufiq2@users.noreply.github.com>
2024-09-12 14:01:02 +0530

    working on confg

--- vite.config.js
+++ vite.config.js
@@ -6,43 +6,47 @@
 //   plugins: [react()],
 // })
 
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
-let faviconURL = '/vite.svg'
-
-
+let faviconURL = "/vite.svg";
 
 export default defineConfig({
-  base: '/boardmeeting',
-  plugins: [react(),
+  base: "/boardmeeting",
+  plugins: [
+    react(),
     VitePWA({
-      includeAssets: [faviconURL],
+      includeAssets: [
+        faviconURL,
+        "src/assets/img/*", // Include all images in the img folder
+      ],
       manifest: {
-        theme_color: '#ffffff',
+        theme_color: "#ffffff",
         icons: [
           {
             src: faviconURL,
-            sizes: '512x512',
-            type: 'image/svg+xml',
-            purpose: 'any maskable'
+            sizes: "512x512",
+            type: "image/svg+xml",
+            purpose: "any maskable",
           },
           {
             src: faviconURL,
-            sizes: '512x512',
-            type: 'image/png',
-          }
-        ]
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
       },
-    })],
+    }),
+  ],
   server: {
     proxy: {
-      '/BoardMeetingApi': {
-        target: 'https://myzonebeta.motilaloswal.com',
+      "/BoardMeetingApi": {
+        target: "https://myzonebeta.motilaloswal.com",
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/BoardMeetingApi/, '/BoardMeetingApi'),
+        rewrite: (path) =>
+          path.replace(/^\/BoardMeetingApi/, "/BoardMeetingApi"),
       },
     },
   },
-})
+});
